c594653c0bbd7fa880514161dbe2c8b96b5fb0f1
Glen1986 <escobar.glen@gmail.com>
2022-01-18 16:48:51 -0300

    toolkit2

--- src/store/Layout/actions.test.js
+++ src/store/Layout/actions.test.js
@@ -1,21 +1,21 @@
-import { Types, Creators} from './index'
+import { hideMessage, showMessage} from './index'
 
 describe('Layout Actions', () =>{
 	describe('Actions Types', ()=>{
 		test('debe retornar Action Type SHOW_MESSAGE',()=>{
-			expect(Types.SHOW_MESSAGE).toEqual('SHOW_MESSAGE');
+			expect(showMessage.type).toEqual('SHOW_MESSAGE');
 		} );
 		test('debe retornar Action Type HIDE_MESSAGE',()=>{
-			expect(Types.HIDE_MESSAGE).toEqual('HIDE_MESSAGE');
+			expect(hideMessage.type).toEqual('HIDE_MESSAGE');
 		} );
 
 	});
 	describe('Actions, Creators', ()=> {
 		test('debe retornar action creator showMessage { type: SHOW_MESSAGE }', ()=>{
-			expect( Creators.showMessage()).toEqual({type: Types.SHOW_MESSAGE})
+			expect( showMessage()).toEqual({type: 'SHOW_MESSAGE'})
 		});
 		test('debe retornar action creator hideMessage { type: HIDE_MESSAGE }', ()=>{
-			expect(Creators.hideMessage()).toEqual({type: Types.HIDE_MESSAGE})
+			expect(hideMessage()).toEqual({type: 'HIDE_MESSAGE'})
 		});
 
 	});
